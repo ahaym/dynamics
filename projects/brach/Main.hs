98c84dc5d8698db7cd691dc01d3a48b6f1c28059
mark <mah6@williams.edu>
2019-04-25 21:43:57 -0400

    brachistochrone

--- projects/brach/Main.hs
+++ projects/brach/Main.hs
@@ -1,10 +1,16 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes #-}
 
+import Data.Maybe (listToMaybe)
 import Data.Reflection
 import Numeric.AD
 import Numeric.AD.Internal.Reverse
 import Numeric.AD.Newton
+import System.Environment
+
+import Graphics.Rendering.Chart.Backend.Cairo
+import Graphics.Rendering.Chart.Easy
+import System.Process
 
 import Lib
 
@@ -18,7 +24,7 @@ zipB start end points = go (start:points)
 foldB :: (Double, Double) -> (Double, Double) -> [(Double, Double)] -> ([(Double, Double)], Double)
 foldB start end points = (start : ans ++ [end], sum . map score . zipB start end $ ans)
     where
-        ans = zip xs (gradientDescent go ys !! 20000)
+        ans = zip xs (gradientDescent go ys !! 5000)
         (x0, y0) = start
         (x1, y1) = end
         (xs, ys) = unzip points
@@ -28,13 +34,22 @@ foldB start end points = (start : ans ++ [end], sum . map score . zipB start end
             . zip (auto <$> xs)
 
         score :: (Num a, Floating a, Mode a) => ((a, a), (a, a)) -> a
-        score ((x0, y0), (x1, y1)) = 2*top / bottom
+        score ((x0, y0), (x1, y1)) = 2*top*bottom
             where
-                top = sqrt $ (x1 - x0)^2 + (y1 - y0)^2
-                bottom = sqrt (1 - y1) + sqrt (1 - y0)
+                grad = (y1 - y0) / (x1 - x0)
+                top = sqrt $ 1 + grad^2
+                bottom = -(sqrt (1 - y1) - sqrt (1 - y0)) / grad
 
 main :: IO ()
 main = do 
-    let (points, ans) = foldB (0, 1) (1, 0.8) [(x, 0.8*(1 - x)) | x <- [0.01,0.02..0.99]]
+    yN <- maybe 1 read . listToMaybe <$> getArgs
+    let (ps, ans) = foldB (0, 1) (1, yN) [(x, 1 - x) | x <- [0.01,0.02..0.99]]
+        --points' = [(x, f x) | x <- [0, 0.01..1]]
+        --f x = (4*(1 - 0.62302128))*(x - 0.5)^2 + 0.62302128
     print ans
-    plotPoints' "gradBrach.png" "Brachistochrone" 2 points
+    toFile def "brachGrad.png" $ do
+        layout_title .= "Brachistochrone"
+        plot (pointsConfig black 3 <$> points "points" ps)
+        --plot (pointsConfig red 5 <$> points "parab" points')
+    callCommand ("feh " ++ "brachGrad.png")
+
